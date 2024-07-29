79947fd5599309a8302fb20e17ffdedbb19efa3a
bpn07 <beepinpathak0721@gmail.com>
2024-07-29 17:15:34 +0545

    Quotation

--- src/app/(main)/quotation/page.tsx
+++ src/app/(main)/quotation/page.tsx
@@ -1,11 +1,88 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Table from "@app/components/Table";
+import { BgImg } from "@app/constants/SvgCollection";
+import logo from "@app/assets/images/logo.png";
 
 const page = () => {
   return (
     <div>
-      Welcome to Quotation Template!
-    </div>
-  )
-}
+      <div className="relative w-full">
+        <div className="absolute w-full h-[346px] overflow-hidden z-10">
+          <div className="object-cover">
+            <BgImg />
+          </div>
+        </div>
+        <div className="relative z-20 w-1/2 pt-12 pb-6 m-auto">
+          <div className="flex justify-between gap-28 text-white uppercase">
+            <div>
+              <div className="w-[133.49px]"> 
+              <Image src={logo} alt="logo" className="w-full"/>
+              </div>
+              <p className="text-6xl font-semibold mt-2">Quotation</p>
+            </div>
+            <div className="text-[10px] font-semibold">
+              <div className="flex justify-start flex-wrap gap-10">
+                <div>
+                  <p>quotation no.</p>
+                  <p className="text-sm font-normal text-[#FFFFFFB2] capitalize">
+                    44832
+                  </p>
+                </div>
+                <div>
+                  <p>created date</p>
+                  <p className="text-sm font-normal text-[#FFFFFFB2] capitalize">
+                    Jun 4, 2024
+                  </p>
+                </div>
+                <div>
+                  <p>invoice to</p>
+                  <p className="text-sm font-normal text-[#FFFFFFB2] capitalize">
+                    Mike Johnson
+                  </p>
+                </div>
+                <div>
+                  <p>phone</p>
+                  <p className="text-sm font-normal text-[#FFFFFFB2] capitalize">
+                    +977-9843229846
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-export default page
+      <div className="relative z-20">
+        <Table />
+      </div>
+
+      <div className="mt-20 w-1/2 m-auto">
+        <div className="flex items-end justify-between">
+          <div className="w-2/3 ">
+            <p className="text-[10px] uppercase text-[#5850EB]">
+              terms & conditions
+            </p>
+            <p className="text-[11px] leading-[17px] mt-3">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking there.
+            </p>
+          </div>
+          <div>
+            <div className="px-8 py-2 border-t border-black text-center">
+              <p className="text-xs font-semibold">Merlyn Devon</p>
+              <p className="text-[11px] text-[#04151ABF]">Associate Manager</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[26px] py-4 text-center bg-[#5850EB0D]">
+        <p className="text-[10px] text-[#04151A]">
+          Please confirm your acceptance of this quote. Thank you!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/Table.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+
+const Table = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="bg-white drop-shadow-md w-1/2 p-4 rounded-2xl">
+        <table className="w-full table-fixed">
+          <thead className="text-left">
+            <tr className="bg-[#DEDCFE] font-semibold text-[10px] text-[#413ACA] uppercase">
+              <th className="px-4 py-2.5 rounded-l-[20px]">Service</th>
+              <th className="py-2.5">Hours</th>
+              <th className="py-2.5">Price{"($)"}</th>
+              <th className="py-2.5 rounded-r-[20px]">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="odd:bg-white even:bg-[#F8F9FC]">
+              <td className="text-xs p-4 rounded-l-[20px] font-semibold">
+                Branding
+                <span className="flex flex-col gap-1 mt-1">
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                </span>
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">04</td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">
+                25,000
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal rounded-r-[20px]">
+                34587
+              </td>
+            </tr>
+            <tr className="odd:bg-white even:bg-[#F8F9FC]">
+              <td className="text-xs p-4 rounded-l-[20px] font-semibold">
+                Branding
+                <span className="flex flex-col gap-1 mt-1">
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                </span>
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">04</td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">
+                25,000
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal rounded-r-[20px]">
+                34587
+              </td>
+            </tr>
+            <tr className="odd:bg-white even:bg-[#F8F9FC]">
+              <td className="text-xs p-4 rounded-l-[20px] font-semibold">
+                Branding
+                <span className="flex flex-col gap-1 mt-1">
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                </span>
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">04</td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">
+                25,000
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal rounded-r-[20px]">
+                34587
+              </td>
+            </tr>
+            <tr className="odd:bg-white even:bg-[#F8F9FC]">
+              <td className="text-xs p-4 rounded-l-[20px] font-semibold">
+                Branding
+                <span className="flex flex-col gap-1 mt-1">
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                </span>
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">04</td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">
+                25,000
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal rounded-r-[20px]">
+                34587
+              </td>
+            </tr>
+            <tr className="odd:bg-white even:bg-[#F8F9FC]">
+              <td className="text-xs p-4 rounded-l-[20px] font-semibold">
+                Branding
+                <span className="flex flex-col gap-1 mt-1">
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                  <span className="text-[11px] text-[#04151ABF] font-normal">
+                    Some service description
+                  </span>
+                </span>
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">04</td>
+              <td className="text-[11px] text-[#04151ABF] font-normal">
+                25,000
+              </td>
+              <td className="text-[11px] text-[#04151ABF] font-normal rounded-r-[20px]">
+                34587
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="p-4 pr-9 bg-[#DEDCFE80] rounded-[20px]">
+          <div className="flex justify-between">
+            <div className="w-1/2">
+              <p className="uppercase text-[10px] text-[#5850EB] font-bold">
+                Payment Info
+              </p>
+              <div className="mt-3">
+                <p className="text-[11px] font-normal mb-2">
+                  Account No. :{" "}
+                  <span className="font-semibold">423756495744832</span>
+                </p>
+                <p className="text-[11px] font-normal">
+                  Bank Name :{" "}
+                  <span className="font-semibold"> HBL Bank Ltd.</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="uppercase w-1/3">
+              <div className="px-4 font-medium">
+                <div className="flex justify-between items-center mb-2">
+                  <p className="text-[10px]">sub total</p>
+                  <p className="text-[11px]">34587</p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-[10px]">tax</p>
+                  <p className="text-[11px]">34587</p>
+                </div>
+              </div>
+              <div className="text-white bg-[#5850EB] rounded-[20px] mt-3 px-4 py-3">
+                <div className="flex justify-between items-center font-bold">
+                  <p className="text-[10px]">total price</p>
+                  <p className="text-[11px]">34587</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
