35a0f954c73210de563fcaceb0d7878e1a514a3c
Prakash Acharya <acharyaprakash030@gmail.com>
2024-08-12 23:42:20 +0545

    update on quotation

---
+++ server/src/utils/client.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export const getKeys = (keys: Array<string>) =>
+  keys.reduce((obj, k) => ({ ...obj, [k]: true }), {});
+
+export default prisma;
